67cdfeec7bc7ad1f4abf34b5f152527dc7554063
classcodingid <saefulmujab.restuikami@gmail.com>
2023-06-14 15:20:05 +0700

    update package populer

--- referensi/site.sh
+++ referensi/site.sh
@@ -1,4 +1,6 @@
 https://flutter.dev/
-https://pub.dev/
+https://pub.dev/packages?q=sdk%3Aflutter+is%3Adart3-compatible+is%3Aflutter-favorite
 https://capekngoding.com/docs/#/
 https://www.freecodecamp.org/news/how-to-build-a-simple-login-app-with-flutter/
+https://www.freecodecamp.org/news/navigation-in-flutter/
+https://www.freecodecamp.org/news/learn-state-management-in-flutter/
---
+++ referensi/teknologi.sh
@@ -0,0 +1,10 @@
+frontend
+1. dart
+2. flutter
+
+
+backend
+1. laravel
+
+dbms
+1. mariadb
